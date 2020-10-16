eabac1c64a2e2a724f5fcebdd1e2bb23000fb6f3
Evgeny Pavliuk <bithix669@gmail.com>
2020-10-16 23:12:04 +0300

    labirint

--- xml_lab.py
+++ xml_lab.py
@@ -3,14 +3,15 @@ from bs4 import BeautifulSoup
 import json as js
 
 
-class base_parsing:
+class Base:
     def __init__(self, sitemap_url, json_name):
         self.book_list = []
+        self.book_data = []
         self.sitemap = sitemap_url
         self.get_html()
         self.get_url_list()
+        self.get_book_data()
         self.json_dump(json_name)
-
 
     def get_html(self, url=None):
         try:
@@ -26,19 +27,19 @@ class base_parsing:
     def get_url_list(self):
         NotImplemented
 
-
+    def get_book_data(self):
+        NotImplemented
 
     def json_dump(self, json_name):
         with open(f"{json_name}.json", "w", encoding='utf8') as jsf:
-            js.dump(self.book_list, jsf)
+            js.dump(self.book_data, jsf)
         jsf.close()
 
 
-class labirint_pars(base_parsing):
+class Labirint(Base):
 
     def __init__(self):
         super().__init__()
-
 
     def get_url_list(self):
         soup = BeautifulSoup(self.sitemap_xml, 'xml')
@@ -60,52 +61,98 @@ class labirint_pars(base_parsing):
                     self.book_list.append(book_url_mod)
             print("Готово")
 
-class book_data(labirint_pars):
+    def get_book_data(self):
+        for book in self.book_list:
+            html = get_html(book['book'])
+            soup = BeautifulSoup(html, 'html.parser')
+            try:
+                title = soup.find('div', class_='prodtitle').find('h1').text
+            except(AttributeError):
+                title = None
+            try:
+                author = soup.find('div', class_='product-description').find('div', class_='authors').find('a').text
+            except(AttributeError):
+                author = None
+            try:
+                other_creators = None
+            except(AttributeError):
+                other_creators = None
+            try:
+                country = None
+            except(AttributeError):
+                country= None
+            try:
+                original_language = None
+            except(AttributeError):
+                original_language= None
+            try:
+                edition_language = None
+            except(AttributeError):
+                edition_language= None
+            try:
+                year_of_creating = None
+            except(AttributeError):
+                year_of_creating= None
+            try:
+                year_of_edition = ''.join(i for i in soup.find('div', class_='product-description').text if isdigit(i))
+            except(AttributeError):
+                year_of_edition= None
+            try:
+                ISBN =''.join(i for i in soup.find('div', class_='product-description').find('div', class_='isbn').text if isdigit(i))
+            except(AttributeError):
+                ISBN= None
+            try:
+                publishing =''.join(i for i in  soup.find('div', class_='product-description').find('div', class_='publisher').find('a').text if isalpha(i))
+            except(AttributeError):
+                publishing= None
+            try:
+                pages_count =''.join(i for i in soup.find('div', class_='product-description').find('div', class_='pages2').text[9:13] if isdigit(i))
+            except(AttributeError):
+                pages_count= None
+            try:
+                cover = soup.find('div', id='product-image').find('img')['src']
+            except(AttributeError):
+                cover= None
+            try:
+                scans = soup.find('img', class_='fotorama__img')
+            except(AttributeError):
+                scans= None
+            try:
+                annotation = soup.find('div', id='product-about').text
+            except(AttributeError):
+                annotation= None
+            try:
+                price = soup.find('span', class_='buying-priceold-val-number').text
+            except(AttributeError):
+                price= None
+            try:
+                sale_price = soup.find('span', class_='buying-pricenew-val-number').text
+            except(AttributeError):
+                sale_price= None
+            last_update = book['lastmod']
+            shop_url = book['book']
 
-    def get_book_data(book):
-        html = get_html(book['book'])
-        soup = BeautifulSoup(html, 'html.parser')
-        data = soup.find('div', class_='product-description')
-        title = soup.find('div', class_='prodtitle').find('h1').text
-        author = data.find('div', class_='authors').find('a').text
-        other_creators = None
-        country = None
-        original_language = None
-        edition_language = None
-        year_of_creating = None
-        year_of_edition = data.find('div', class_='publisher').text[-7:-3]
-        ISBN = data.find('div', class_='isbn').text[6:]
-        publishing = data.find('div', class_='publisher').find('a').text
-        pages_count = data.find('div', class_='pages2').text[9:13]
-        cover = soup.find('div', id='product-image').find('img')['src']
-        scans = soup.find('img', class_='fotorama__img')
-        annotation = soup.find('div', id='product-about').text
-        last_update = book['lastmod']
-        shop_url = book['book']
-        price = soup.find('span', class_='buying-priceold-val-number').text
-        sale_price = soup.find('span', class_='buying-pricenew-val-number').text
-
-        book_data = {
-            'title': title,
-            'author': author,
-            'other_creators': other_creators,
-            'country': country,
-            'original_language': original_language,
-            'edition_language': edition_language,
-            'year_of_creating': year_of_creating,
-            'year_of_edition': year_of_edition,
-            'ISBN': ISBN,
-            'publishing': publishing,
-            'pages_count': pages_count,
-            'cover': cover,
-            'scans': scans,
-            'annotation': annotation,
-            'last_update': last_update,
-            'shop_url': shop_url,
-            'price': price,
-            'sale_price': sale_price
-        }
-        print(book_data)
+            book_data = {'title': title, 'data': {
+                'title': title,
+                'author': author,
+                'other_creators': other_creators,
+                'country': country,
+                'original_language': original_language,
+                'edition_language': edition_language,
+                'year_of_creating': year_of_creating,
+                'year_of_edition': year_of_edition,
+                'ISBN': ISBN,
+                'publishing': publishing,
+                'pages_count': pages_count,
+                'cover': cover,
+                'scans': scans,
+                'annotation': annotation,
+                'last_update': last_update,
+                'shop_url': shop_url,
+                'price': price,
+                'sale_price': sale_price
+            }}
+            self.book_data.append(book_data)
 
 
 labirint_pars('https://www.labirint.ru/sitemap.xml', 'labirint')
@@ -152,7 +199,6 @@ labirint_pars('https://www.labirint.ru/sitemap.xml', 'labirint')
 # with open("Labirint_url.json", "w", encoding='utf8') as jsf:
 #     js.dump(book_list, jsf)
 # jsf.close()
-
 
 
 dict = {"book": "https://www.labirint.ru/books/746088/", "lastmod": "2020-09-26T01:45:47+03:00"}
